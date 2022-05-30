0f711053c9ac8da0136a37a090a6a1d8c711255d
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-05-29 21:17:52 -0500

    add css imports

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ('../App.css');
+import ('./footer.css');
 
 function Footer(props) {
     return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Navbar from './Navbar';
 import ('../App.css');
+import ('./header.css');
 
 function Header(props) {
     return (
         <div className="header-basic">
         <header>
+            <Navbar />
             <div className="column">
                 <h1>Sadie</h1>
                 <h2>Full-Stack Web Developer</h2>
